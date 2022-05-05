3e3111fd887234c4abc57f4a2bb8ef2ef146a72c
mgarcia-talavera <mgarcia-talavera@uoc.edu>
2022-05-05 15:25:00 +0200

    PEC2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,13 +2,14 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
-
-// import 'some-node-module';
+ // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-
+import * as bootstrap from 'bootstrap';
 /**
  * Write any other JavaScript below
  */
+
+
 
 +( function() {
   const university = "UOC";
